38a7f94078adc0972a4e2934f0a7e9de24811c94
亨睿 <henry.lx@alibaba-inc.com>
2019-08-30 12:04:51 +0800

    Remove Manufacturer

--- src/conf.h
+++ src/conf.h
@@ -2,7 +2,6 @@
 
 // Discoverable Bluetooth device name.
 #define DEVICE_NAME "Henry's AirCharger"
-#define DEVICE_MANUFACTURER "Henry Li"
 
 // Comment the next line to disable all Bluetooth related features.
 // This is very handy when you only want to debug OLED or other
