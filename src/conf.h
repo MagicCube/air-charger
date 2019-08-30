8d5036e10f60ef4c508e44bfe387030af4b96e61
亨睿 <henry.lx@alibaba-inc.com>
2019-08-30 11:41:13 +0800

    Add DEVICE_MANUFACTURER and BLE_ENABLED

--- src/conf.h
+++ src/conf.h
@@ -1,1 +1,10 @@
+#pragma once
+
+// Discoverable Bluetooth device name.
 #define DEVICE_NAME "Henry's AirCharger"
+#define DEVICE_MANUFACTURER "Henry Li"
+
+// Comment the next line to disable all Bluetooth related features.
+// This is very handy when you only want to debug OLED or other
+// non-BLE features.
+#define BLE_ENABLED
