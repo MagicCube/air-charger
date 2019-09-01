8244254a799916328538587881c535e4f65bc319
Henry Li <henry1943@163.com>
2019-09-01 17:40:44 +0800

    Don't use grey color

--- src/displaying/scenes/MainScene.cpp
+++ src/displaying/scenes/MainScene.cpp
@@ -69,8 +69,7 @@ void MainScene::_drawBattery(TFT_eSPI *canvas) {
   float batteryLevel = BLEPeripheral.getRemoteDevice()->batteryLevel();
   int batteryWidth = ((float)(_batteryFrame.width - BATTERY_HEAD_WIDTH - BATTERY_PADDING * 2 - 3)) *
                      (batteryLevel / 100);
-  // auto batteryColor = TFT_DARKGREEN;
-  auto batteryColor = 0x4228; // grey
+  auto batteryColor = TFT_DARKGREEN;
   if (batteryLevel <= 20) {
     batteryColor = TFT_ORANGE;
   } else if (batteryLevel <= 10) {
