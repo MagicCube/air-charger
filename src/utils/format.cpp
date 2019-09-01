9585f383367cee79ab96aae5e234a41bef6e915e
Henry Li <henry1943@163.com>
2019-09-01 15:10:59 +0800

    Add date & time formating

--- src/utils/format.cpp
+++ src/utils/format.cpp
@@ -6,3 +6,24 @@ String formatBLEAddress(ble_address_t bytes) {
           bytes[5]);
   return String(result);
 }
+
+String formatTime(DateTime dateTime) {
+  char result[10];
+  sprintf(result, "%02d:%02d:%02d", dateTime.getHours(), dateTime.getMinutes(),
+          dateTime.getSeconds());
+  return String(result);
+}
+
+String formatDate(DateTime dateTime) {
+  char result[12];
+  sprintf(result, "%d-%02d-%02d", dateTime.getFullYear(), dateTime.getMonth() + 1,
+          dateTime.getDate());
+  return String(result);
+}
+
+String formatDateTime(DateTime dateTime) {
+  char result[24];
+  sprintf(result, "%d-%02d-%02d %02d:%02d:%02d", dateTime.getFullYear(), dateTime.getMonth() + 1,
+          dateTime.getDate(), dateTime.getHours(), dateTime.getMinutes(), dateTime.getSeconds());
+  return String(result);
+}
--- src/utils/format.h
+++ src/utils/format.h
@@ -3,5 +3,9 @@
 #include <Arduino.h>
 
 #include "../ble/ble_address_t.h"
+#include "../timing/DateTime.h"
 
 String formatBLEAddress(ble_address_t bytes);
+String formatDate(DateTime dateTime);
+String formatTime(DateTime dateTime);
+String formatDateTime(DateTime dateTime);
