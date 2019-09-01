9d61e18d66efcd13ea5d9f6f74d93d3c093dac3a
Henry Li <henry1943@163.com>
2019-09-01 15:09:23 +0800

    Split into two files

---
+++ src/timing/DateTime.cpp
@@ -0,0 +1,65 @@
+#include "DateTime.h"
+
+#include "conf.h"
+#include "log.h"
+
+static time_t __currentTimeOffsetTicks = 0;
+
+DateTime::DateTime(time_t time) {
+  _time = time;
+  _localTime = localtime(&_time);
+}
+
+DateTime::DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t min,
+                   uint8_t sec) {
+  tm localTime;
+  localTime.tm_year = year - 1900;
+  localTime.tm_mon = month;
+  localTime.tm_mday = day;
+  localTime.tm_hour = hour;
+  localTime.tm_min = min;
+  localTime.tm_sec = sec;
+  localTime.tm_isdst = -1;
+  _time = mktime(&localTime) - TIME_ZONE_OFFSET * 60;
+  _localTime = localtime(&_time);
+}
+
+void DateTime::configTime(DateTime time) {
+  __currentTimeOffsetTicks = time.gmtTime() - millis() / 1000;
+}
+
+time_t DateTime::now() {
+  return __currentTimeOffsetTicks + millis() / 1000;
+}
+
+int DateTime::getFullYear() {
+  return _localTime->tm_year + 1900;
+}
+
+int DateTime::getMonth() {
+  return _localTime->tm_mon;
+}
+
+int DateTime::getDate() {
+  return _localTime->tm_mday;
+}
+
+int DateTime::getDay() {
+  return _localTime->tm_wday;
+}
+
+int DateTime::getHours() {
+  return _localTime->tm_hour;
+}
+
+int DateTime::getMinutes() {
+  return _localTime->tm_min;
+}
+
+int DateTime::getSeconds() {
+  return _localTime->tm_sec;
+}
+
+time_t DateTime::gmtTime() {
+  return _time;
+}
--- src/timing/DateTime.h
+++ src/timing/DateTime.h
@@ -3,70 +3,24 @@
 #include <Arduino.h>
 #include <time.h>
 
-#include "conf.h"
-#include "log.h"
-
-static time_t currentTimeOffsetTicks = 0;
-
 struct DateTime {
-  DateTime(time_t time) {
-    _time = time;
-    _localTime = gmtime(&_time);
-  }
+public:
+  DateTime(time_t time);
 
-  DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t min, uint8_t sec) {
-    tm localTime;
-    localTime.tm_year = year - 1900;
-    localTime.tm_mon = month;
-    localTime.tm_mday = day;
-    localTime.tm_hour = hour;
-    localTime.tm_min = min;
-    localTime.tm_sec = sec;
-    localTime.tm_isdst = -1;
-    _time = mktime(&localTime) - TIME_ZONE_OFFSET * 60;
-    _localTime = localtime(&_time);
-  }
+  DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t min, uint8_t sec);
 
-  static void configTime(DateTime time) {
-    currentTimeOffsetTicks = time.gmtTime() - millis() / 1000;
-  }
+  static void configTime(DateTime time);
+  static time_t now();
+  int getFullYear();
+  int getMonth();
+  int getDate();
+  int getDay();
+  int getHours();
+  int getMinutes();
+  int getSeconds();
+  time_t gmtTime();
 
-  static time_t now() {
-    return currentTimeOffsetTicks + millis() / 1000;
-  }
-
-  int getFullYear() {
-    return _localTime->tm_year + 1900;
-  }
-
-  int getMonth() {
-    return _localTime->tm_mon;
-  }
-
-  int getDate() {
-    return _localTime->tm_mday;
-  }
-
-  int getDay() {
-    return _localTime->tm_wday;
-  }
-
-  int getHours() {
-    return _localTime->tm_hour;
-  }
-
-  int getMinutes() {
-    return _localTime->tm_min;
-  }
-
-  int getSeconds() {
-    return _localTime->tm_sec;
-  }
-
-  time_t gmtTime() {
-    return _time;
-  }
-
+private:
   time_t _time;
   struct tm *_localTime;
 };
