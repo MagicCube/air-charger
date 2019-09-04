a83120dacd8d827fd6f063af700fa226a2f5583d
Henry Li <henry1943@163.com>
2019-09-04 22:49:49 +0800

    Bugfix

--- src/scenes/MainScene.cpp
+++ src/scenes/MainScene.cpp
@@ -33,11 +33,9 @@ void MainScene::redraw() {
   _batteryView.redraw(true); // Always force redraw
   _drawChargingIndicator();
 
-  #ifdef DEBUG
   #ifdef DRAW_CONSTRUCTION_LINE
   auto context = TFTDrawingContext::getScreenDrawingContext();
   context->drawCircle(context->bounds().middlePoint(), context->size().width / 2 - 1, DRAW_CONSTRUCTION_LINE);
-  #endif
   #endif
 }
 
