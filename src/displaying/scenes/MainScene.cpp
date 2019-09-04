96af90850d24e70fa871874d70a38a0615960658
亨睿 <henry.lx@alibaba-inc.com>
2019-09-04 09:29:05 +0800

    Refactor: make all public fields as functions

--- src/displaying/scenes/MainScene.cpp
+++ src/displaying/scenes/MainScene.cpp
@@ -17,8 +17,8 @@
 MainScene::MainScene()
     : _clockFrame((TFT_WIDTH - 160) / 2, 40, 160, 52),
       _batteryFrame((240 - 150) / 2 + BATTERY_HEAD_WIDTH / 2 + 2, 128, 160, 60),
-      _chargingIndicatorFrame(_batteryFrame.left - charging_indicator_width - 8,
-                              _batteryFrame.top + 8, charging_indicator_width,
+      _chargingIndicatorFrame(_batteryFrame.left() - charging_indicator_width - 8,
+                              _batteryFrame.top() + 8, charging_indicator_width,
                               charging_indicator_height) {
 }
 
@@ -59,13 +59,13 @@ void MainScene::_drawClock(TFT_eSPI *canvas) {
     _clock = new TFT_eSprite(canvas);
     _clock->setColorDepth(1);
   }
-  _clock->createSprite(_clockFrame.width, _clockFrame.height);
+  _clock->createSprite(_clockFrame.width(), _clockFrame.height());
   _clock->setTextColor(TFT_WHITE);
   _clock->setTextFont(7);
   _clock->setTextSize(1);
   _clock->setTextDatum(CC_DATUM);
   _clock->drawString(timeString, _clock->width() / 2, _clock->height() / 2);
-  _clock->pushSprite(_clockFrame.left, _clockFrame.top);
+  _clock->pushSprite(_clockFrame.left(), _clockFrame.top());
   _clock->deleteSprite();
 }
 
@@ -74,18 +74,18 @@ void MainScene::_drawBattery(TFT_eSPI *canvas) {
     _battery = new TFT_eSprite(canvas);
     _battery->setColorDepth(8);
   }
-  _battery->createSprite(_batteryFrame.width, _batteryFrame.height);
+  _battery->createSprite(_batteryFrame.width(), _batteryFrame.height());
 
-  _battery->drawRoundRect(0, 0, _batteryFrame.width - BATTERY_HEAD_WIDTH - 3, _batteryFrame.height,
+  _battery->drawRoundRect(0, 0, _batteryFrame.width() - BATTERY_HEAD_WIDTH - 3, _batteryFrame.height(),
                           BATTERY_HEAD_WIDTH, TFT_WHITE);
 
-  int headHeight = _batteryFrame.height / 2;
-  _battery->fillRoundRect(_batteryFrame.width - BATTERY_HEAD_WIDTH,
-                          (_batteryFrame.height - headHeight) / 2, BATTERY_HEAD_WIDTH, headHeight,
+  int headHeight = _batteryFrame.height() / 2;
+  _battery->fillRoundRect(_batteryFrame.width() - BATTERY_HEAD_WIDTH,
+                          (_batteryFrame.height() - headHeight) / 2, BATTERY_HEAD_WIDTH, headHeight,
                           BATTERY_ROUNDNESS, TFT_WHITE);
 
   float batteryLevel = BLEPeripheral.getRemoteDevice()->batteryLevel();
-  int batteryWidth = ((float)(_batteryFrame.width - BATTERY_HEAD_WIDTH - BATTERY_PADDING * 2 - 3)) *
+  int batteryWidth = ((float)(_batteryFrame.width() - BATTERY_HEAD_WIDTH - BATTERY_PADDING * 2 - 3)) *
                      (batteryLevel / 100);
   auto batteryColor = Charger.isCharging() ? TFT_DARKGREEN : TFT_GREY;
   if (batteryLevel <= 10) {
@@ -95,27 +95,27 @@ void MainScene::_drawBattery(TFT_eSPI *canvas) {
   }
   auto batteryTextColor = TFT_WHITE;
   _battery->fillRect(BATTERY_PADDING, BATTERY_PADDING, batteryWidth,
-                     _batteryFrame.height - BATTERY_PADDING * 2, batteryColor);
+                     _batteryFrame.height() - BATTERY_PADDING * 2, batteryColor);
 
   _battery->setTextColor(batteryTextColor);
   _battery->setFreeFont(&FreeSansBold18pt7b);
   _battery->setTextSize(1);
   _battery->setTextDatum(CC_DATUM);
   _battery->drawString(formatPercentage(batteryLevel),
-                       (_batteryFrame.width - BATTERY_HEAD_WIDTH - 3) / 2,
-                       _batteryFrame.height / 2);
+                       (_batteryFrame.width() - BATTERY_HEAD_WIDTH - 3) / 2,
+                       _batteryFrame.height() / 2);
 
-  _battery->pushSprite(_batteryFrame.left, _batteryFrame.top);
+  _battery->pushSprite(_batteryFrame.left(), _batteryFrame.top());
   _battery->deleteSprite();
 }
 
 void MainScene::_drawChargingIndicator(TFT_eSPI *canvas) {
   if (Charger.isCharging()) {
-    canvas->drawXBitmap(_chargingIndicatorFrame.left, _chargingIndicatorFrame.top,
-                        charging_indicator_bits, _chargingIndicatorFrame.width,
-                        _chargingIndicatorFrame.height, TFT_WHITE);
+    canvas->drawXBitmap(_chargingIndicatorFrame.left(), _chargingIndicatorFrame.top(),
+                        charging_indicator_bits, _chargingIndicatorFrame.width(),
+                        _chargingIndicatorFrame.height(), TFT_WHITE);
   } else {
-    canvas->fillRect(_chargingIndicatorFrame.left, _chargingIndicatorFrame.top,
-                     _chargingIndicatorFrame.width, _chargingIndicatorFrame.height, TFT_BLACK);
+    canvas->fillRect(_chargingIndicatorFrame.left(), _chargingIndicatorFrame.top(),
+                     _chargingIndicatorFrame.width(), _chargingIndicatorFrame.height(), TFT_BLACK);
   }
 }
--- src/drawing/Rect.h
+++ src/drawing/Rect.h
@@ -4,22 +4,40 @@
 
 // Stores a set of four egers that represent the location and size of a rectangle.
 struct Rect {
-  Rect(uint32_t p_left = 0, uint32_t p_top = 0, uint32_t p_width = 0, uint32_t p_height = 0) {
-    left = p_left;
-    top = p_top;
-    width = p_width;
-    height = p_height;
+public:
+  Rect(uint32_t left = 0, uint32_t top = 0, uint32_t width = 0, uint32_t height = 0) {
+    _left = left;
+    _top = top;
+    _width = width;
+    _height = height;
+  }
+
+  uint32_t left() {
+    return _left;
+  }
+
+  uint32_t top() {
+    return _top;
+  }
+
+  uint32_t width() {
+    return _width;
+  }
+
+  uint32_t height() {
+    return _height;
   }
 
   void copyFrom(Rect rect) {
-    left = rect.left;
-    top = rect.top;
-    width = rect.width;
-    height = rect.height;
+    _left = rect.left();
+    _top = rect.top();
+    _width = rect.width();
+    _height = rect.height();
   }
 
-  uint32_t left = 0;
-  uint32_t top = 0;
-  uint32_t width = 0;
-  uint32_t height = 0;
+private:
+  uint32_t _left = 0;
+  uint32_t _top = 0;
+  uint32_t _width = 0;
+  uint32_t _height = 0;
 };
